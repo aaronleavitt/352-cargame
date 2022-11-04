b819a8bb8c0ed13d2c6c7cd1ecac054062fcaecc
aaronleavitt <aaronm.leavitt@gmail.com>
2022-11-04 14:23:08 -0400

    added serialized move speed and steer speed

---
+++ Assets/Driver.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Driver : MonoBehaviour
+{
+
+        [SerializeField] float steerSpeed = 0.75f;
+        [SerializeField] float moveSpeed = .01f;
+
+    void Start()
+    {
+
+    }
+
+
+    void Update()
+    {
+        
+        transform.Rotate(0,0,steerSpeed);
+        transform.Translate(0,moveSpeed,0);
+      
+    }
+}
